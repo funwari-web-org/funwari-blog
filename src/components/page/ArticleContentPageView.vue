c0f9429b0809cbd82798f6cbebbd6c3695ca79d4
megatkhs <tarodino0526@gmail.com>
2021-12-20 23:33:50 +0900

    [change] ArticleContentPageからViewの責務をArticleContentPageViewに分離

---
+++ src/components/page/ArticleContentPageView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="doc">
+    <div class="doc__content">
+      <AppDocArticle class="doc__article" :document="document" />
+    </div>
+    <aside class="doc__side">
+      <AppDocToc class="doc__toc" :toc="document.toc" />
+    </aside>
+  </div>
+</template>
+
+<script lang="ts">
+import { FetchReturn } from '@nuxt/content/types/query-builder'
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'ArticleContentPageView',
+  props: {
+    document: {
+      type: Object as PropType<FetchReturn>,
+      require: true,
+    },
+    prevDocument: {
+      type: Object as PropType<FetchReturn>,
+      default: undefined,
+    },
+    nextDocument: {
+      type: Object as PropType<FetchReturn>,
+      default: undefined,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@use '~/assets/style/global';
+
+.doc {
+  display: flex;
+
+  &__content {
+    width: 70%;
+  }
+
+  &__side {
+    width: 30%;
+    padding-left: 30px;
+  }
+
+  &__toc {
+    position: sticky;
+    top: 100px;
+  }
+}
+</style>
--- src/pages/articles/_.vue
+++ src/pages/articles/_.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="doc">
-    <div class="doc__content">
-      <AppDocArticle class="doc__article" :document="document" />
-    </div>
-    <aside class="doc__side">
-      <AppDocToc class="doc__toc" :toc="document.toc" />
-    </aside>
-  </div>
+  <ArticleContentPageView
+    :document="document"
+    :prevDocument="prevDocument"
+    :nextDocument="nextDocument"
+  />
 </template>
 
 <script lang="ts">
@@ -37,31 +34,17 @@ export default defineComponent({
       return
     }
 
+    const [prevDocument, nextDocument] = await $content('articles')
+      .only(['title', 'publishedAt', 'thumbnail', 'path'])
+      .sortBy('publishedAt', 'desc')
+      .surround(document.slug)
+      .fetch() as FetchReturn[]
+
     return {
       document,
+      prevDocument,
+      nextDocument,
     }
   },
 })
 </script>
-
-<style lang="scss" scoped>
-@use '~/assets/style/global';
-
-.doc {
-  display: flex;
-
-  &__content {
-    width: 70%;
-  }
-
-  &__side {
-    width: 30%;
-    padding-left: 30px;
-  }
-
-  &__toc {
-    position: sticky;
-    top: 100px;
-  }
-}
-</style>
